61d6c774b068e713f5ec62ed2cecb7e3e1c5096d
Ignacio Benedetti <tranceway@gmail.com>
2014-02-26 15:09:21 -0300

    Pruebas de barcode

--- js/index.js
+++ js/index.js
@@ -55,24 +55,16 @@ var app = {
 
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        scanner.scan( function (result) {
+        scanner.scan(function(data) {
+            if (result.cancelled === true) {
+                return;
+            }
 
             alert("We got a barcode\n" +
             "Result: " + result.text + "\n" +
             "Format: " + result.format + "\n" +
             "Cancelled: " + result.cancelled);
 
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
 
         }, function (error) {
             console.log("Scanning failed: ", error);
