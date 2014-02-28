7d3ce1ed0938eaa13bc50c9dfc4f2e45896d406c
Ignacio Benedetti <tranceway@gmail.com>
2014-02-28 14:54:58 -0300

    más cosas

--- js/requests.js
+++ js/requests.js
@@ -1,4 +1,40 @@
 $(document).on("pagecreate", "#principal", function() {
+
+    var test = function() {
+        var $ul = $("#sucursales_cercanas_listview"),
+        html = "";
+
+        console.log($ul);
+        $.ajax({
+            url: "http://localhost:8000/api/v1/sucursales/",
+            dataType: "jsonp",
+            crossDomain: true,
+            data: {
+                lat: -38.7316685,
+                lon: -62.251555,
+                limit: 3
+            }
+        })
+        .then(function(response) {
+            console.log(response);
+            if (response.count > 0){
+                $.each(response.results, function (i, obj) {
+                    html += '<li><a href="?id=' + obj.id + '/#sucursal">' + obj.nombre + '</a></li>';
+                });
+            }
+            else {
+                html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
+            }
+
+            $ul.html( html );
+            $ul.listview( "refresh" );
+            $ul.trigger( "updatelayout");
+        });
+    }
+    test();
+
+    // --
+
     $("#sucursales_listview").on("filterablebeforefilter", function (e, data) {
         var $ul = $( this ),
             $input = $( data.input ),
@@ -18,19 +54,19 @@ $(document).on("pagecreate", "#principal", function() {
                     q: $input.val()
                 }
             })
-            .then(function(data) {
-                if (data.count > 0){
-                    $.each(data.results, function (i, obj) {
-                        html += '<li><a href="#sucursal?id=' + obj.id + '">' + obj.nombre + '</a></li>';
+            .then(function(response) {
+                if (response.count > 0){
+                    $.each(response.results, function (i, obj) {
+                        html += '<li><a href="?id=' + obj.id + '/#sucursal">' + obj.nombre + '</a></li>';
                     });
-
-                    $ul.html( html );
-                    $ul.listview( "refresh" );
-                    $ul.trigger( "updatelayout");
                 }
                 else {
-                    alert("No hay resultados :(");
+                    html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
                 }
+
+                $ul.html( html );
+                $ul.listview( "refresh" );
+                $ul.trigger( "updatelayout");
             });
         }
     });
