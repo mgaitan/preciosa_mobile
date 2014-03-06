6d687fbfa4b932da030a51feb7223d7aa82595bc
Ignacio Benedetti <tranceway@gmail.com>
2014-03-06 13:07:04 -0300

    Refactorización de peticiones ajax

--- js/requests.js
+++ js/requests.js
@@ -1,74 +1,133 @@
-$(document).on("pagecreate", "#principal", function() {
+var consultar_sucursales = function(callback, params) {
+    if (typeof(params) === 'undefined') params = {};
 
-    var test = function() {
-        var $ul = $("#sucursales_cercanas_listview"),
-        html = "";
+    var data = {
+        format: 'jsonp',
+    }
 
-        console.log($ul);
-        $.ajax({
-            url: "http://localhost:8000/api/v1/sucursales/",
-            dataType: "jsonp",
-            crossDomain: true,
-            data: {
-                lat: -38.7316685,
-                lon: -62.251555,
-                limit: 3,
-                format: 'jsonp',
-            }
-        })
-        .then(function(response) {
-            if (response.count > 0){
-                $.each(response.results, function (i, obj) {
-                    html += '<li><a href="?id='+obj.id+'#sucursal">' + obj.nombre + '</a></li>';
-                });
-            }
-            else {
-                html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
-            }
+    if (typeof(params.lat) !== 'undefined' && typeof(params.lon) !== 'undefined') {
+        data.lat = params.lat;
+        data.lon = params.lon;
+    }
 
-            $ul.html( html );
-            $ul.listview( "refresh" );
-            $ul.trigger( "updatelayout");
+    if (typeof(params.limit) !== 'undefined') {
+        data.limit = params.limit;
+    }
+
+    if (typeof(params.q) !== 'undefined') {
+        data.q = params.q;
+    }
+
+    $.ajax({
+        url: "http://localhost:8000/api/v1/sucursales/",
+        dataType: "jsonp",
+        crossDomain: true,
+        data: data,
+        error: function(xhr, text, error) {
+            return callback('error', text, params.selector);
+        },
+        success: function(response) {
+            return callback('ok', response, params.selector);
+        },
+    })
+};
+
+var consultar_productos = function(callback, params) {
+    if (typeof(params) === 'undefined') params = {};
+
+    var data = {
+        format: 'jsonp',
+    }
+
+    if (typeof(params.barcode) !== 'undefined') {
+        data.barcode = params.barcode;
+    }
+
+    if (typeof(params.q) !== 'undefined') {
+        data.q = params.q;
+    }
+
+    $.ajax({
+        url: "http://localhost:8000/api/v1/productos/",
+        dataType: "jsonp",
+        crossDomain: true,
+        data: data,
+        error: function(xhr, text, error) {
+            return callback('error', text, params.selector);
+        },
+        success: function(response) {
+            return callback('ok', response, params.selector);
+        },
+    })
+};
+
+var mostrar_sucursales = function(status, response, selector) {
+    var $ul = selector,
+    html = '';
+
+    if (response.count > 0){
+        $.each(response.results, function (i, obj) {
+            html += '<li><a href="/?id='+obj.id+'#sucursal">' + obj.nombre + '</a></li>';
         });
     }
-    test();
+    else {
+        html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
+    }
 
-    // --
+    $ul.html(html);
+    $ul.listview('refresh');
+    $ul.trigger('updatelayout');
+};
+
+var mostrar_productos = function(status, response, selector) {
+    var $ul = selector,
+    html = '';
+
+    if (response.count > 0){
+        $.each(response.results, function (i, obj) {
+            html += '<li><a href="?id='+obj.id+'#producto">' + obj.descripcion + ' <i>[' + obj.upc + ']</i></a></li>';
+        });
+    }
+    else {
+        html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
+    }
+
+    $ul.html(html);
+    $ul.listview('refresh');
+    $ul.trigger('updatelayout');
+}
+
+// ---
+
+$(document).on("pagecreate", "#principal", function() {
+    consultar_sucursales(
+        mostrar_sucursales,
+        {
+            selector: $('#sucursales_cercanas_listview'),
+            lat: -38.7316685,
+            lon: -62.251555,
+            limit: 3
+        }
+    );
 
     $("#sucursales_listview").on("filterablebeforefilter", function (e, data) {
         var $ul = $( this ),
             $input = $( data.input ),
             value = $input.val(),
-            html = "";
+            html = '';
 
-        $ul.html( "" );
+        $ul.html('');
         if (value && value.length > 2) {
-            $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
-            $ul.listview( "refresh" );
+            $ul.html( '<li><div class="ui-loader"><span class="ui-icon ui-icon-loading"></span></div></li>');
+            $ul.listview('refresh');
 
-            $.ajax({
-                url: "http://localhost:8000/api/v1/sucursales/",
-                dataType: "jsonp",
-                crossDomain: true,
-                data: {
-                    q: $input.val(),
-                    format: 'jsonp'
+            consultar_sucursales(
+                mostrar_sucursales,
+                {
+                    selector: $ul,
+                    q: $input.val()
                 }
-            })
-            .then(function(response) {
-                if (response.count > 0){
-                    $.each(response.results, function (i, obj) {
-                        html += '<li><a href="?id='+obj.id+'#sucursal">' + obj.nombre + '</a></li>';
-                    });
-                }
-                else {
-                    html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
-                }
-
-                $ul.html( html );
-                $ul.listview( "refresh" );
-                $ul.trigger( "updatelayout");
-            });
+            );
         }
     });
 });
@@ -78,40 +137,20 @@ $(document).on("pagecreate", "#sucursal", function() {
         var $ul = $( this ),
             $input = $( data.input ),
             value = $input.val(),
-            html = "";
+            html = '';
 
-        $ul.html( "" );
+        $ul.html('');
         if (value && value.length > 2) {
-            $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
-            $ul.listview( "refresh" );
+            $ul.html('<li><div class="ui-loader"><span class="ui-icon ui-icon-loading"></span></div></li>');
+            $ul.listview('refresh');
 
-            $.ajax({
-                url: "http://localhost:8000/api/v1/productos/",
-                dataType: "jsonp",
-                crossDomain: true,
-                data: {
-                    q: $input.val(),
-                    format: 'jsonp'
+            consultar_productos(
+                mostrar_productos,
+                {
+                    selector: $ul,
+                    q: $input.val()
                 }
-            })
-            .then(function(response) {
-                if (response.count > 0){
-                    $.each(response.results, function (i, obj) {
-                        html += '<li><a href="?id='+obj.id+'#producto">' + obj.descripcion + ' <i>[' + obj.upc + ']</i></a></li>';
-                    });
-                }
-                else {
-                    html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No se encontraron resultados</a></li>';
-                }
-
-                $ul.html( html );
-                $ul.listview( "refresh" );
-                $ul.trigger( "updatelayout");
-            });
+            )
         }
     });
-});
-
-$('producto').on('pagechange', function(){
-    alert(111);
 });
