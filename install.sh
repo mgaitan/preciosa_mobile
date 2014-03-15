2f364e046ef8a4b4c9a04e604df1176173195aba
Martín Gaitán <gaitan@gmail.com>
2014-03-14 21:09:21 -0300

    comment webhook

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
- #!/bin/sh
- wget https://build.phonegap.com/apps/762757/download/android
- mv android preciosa.apk
- adb install -r preciosa.apk
- rm preciosa.apk
+#!/bin/sh
+#
+# Cada vez que se hace un push, github hace un post a la API
+# de phonegap para que recompile la aplicación.
+#
+# https://build.phonegap.com/apps/[appid]/build/?auth_token=[token]
+#
+wget https://build.phonegap.com/apps/762757/download/android
+mv android preciosa.apk
+adb install -r preciosa.apk
+rm preciosa.apk
