fb5edd54ae2f292c79e97575b41ce0f98c0c2868
Martín Gaitán <gaitan@gmail.com>
2014-03-17 15:01:28 -0300

    experimento github webhook

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Github hace un post a la direccion
-# https://build.phonegap.com/apps/<APP_ID>/build/?auth_token=<TOKEN>&pull=true
+# https://build.phonegap.com/apps/<APP_ID>/push/?auth_token=<TOKEN>
 #
 wget https://build.phonegap.com/apps/762757/download/android
 mv android preciosa.apk
