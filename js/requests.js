377895cfd791a79673aa7d7f5f162a6c19fb35ba
Martín Gaitán <gaitan@gmail.com>
2014-03-17 14:45:18 -0300

    geolocalizacion de la ubicacion actual

--- js/requests.js
+++ js/requests.js
@@ -267,3 +267,36 @@ $(document).on('pageinit', '#principal', function(){
 $(document).on('pageinit', '#sucursal', function(){
     $(document).on('click', 'a.producto', asignar_producto_id);
 });
+
+$(document).on('pageinit', '#ubicacion', function(){
+    // Default en el bunker de Preciosa bunker
+    var defaultLatLng = new google.maps.LatLng(-31.4428, -64.1797);
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 15,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "acá estás"
+        });
+    }
+
+});
