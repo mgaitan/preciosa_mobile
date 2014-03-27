cb15bcacf72c099360d48fdfa522d08531033069
Martín Gaitán <gaitan@gmail.com>
2014-03-27 01:49:35 -0300

    es dificil el debugging en el celu

--- js/requests.js
+++ js/requests.js
@@ -276,8 +276,8 @@ $(document).on("pagecreate", "#principal", function() {
             html = '<li><a class="ui-btn ui-shadow ui-corner-all ui-icon-alert ui-btn-icon-left">No hay sucursales recientes</a></li>';
         }
 
-        // para test en el mobil
-        alert($('a:first', $('<div></div>').append(html)).attr('class'));
+        // para test en el movil
+        console.log('### CLASSES' + $('a:first', $('<div></div>').append(html)).attr('class'));
 
         actualizar_listview(html, $('#sucursales_recientes_listview'));
     });
